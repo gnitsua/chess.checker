cb81a5e5bb36fa8a5649847f73c95a6a94d3f004
mudkipmaster <acg59@drexel.edu>
2018-08-02 18:02:28 -0400

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,19 @@
+import com.example.HelloWorld;
+import org.junit.Assert;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MyTest {
+    @Test
+    public void name() throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        HelloWorld.print(new PrintStream(out));
+        String s = out.toString();
+        Assert.assertEquals("Hello", s);
+
+    }
+
+}
